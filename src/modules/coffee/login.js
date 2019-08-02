93c6531c2592ab7ba6dacb9b6f4b8c240046276f
nhatanh17 <anhlnctk40@gmail.com>
2019-08-02 14:58:21 +0700

    LoginPage_NhatAnh

--- src/modules/coffee/login.js
+++ src/modules/coffee/login.js
@@ -1,4 +1,6 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
+import './styles.css'
 class LoginCoffee extends React.Component {
 
     state = {
@@ -9,7 +11,30 @@ class LoginCoffee extends React.Component {
     render() {
         return (
             <Fragment>
-               <h1>Login coffee</h1>
+                <div className="container-login">
+                    <div className="wrap-login">
+                        <span className="login-form-title">Account Login</span>
+                        <form className="login-form">
+                            <InputGroup>
+                                <InputGroupAddon addonType="prepend">
+                                    <span className="input-group-text" id="basic-addon">
+                                    <i class="fas fa-user"></i>
+                                    </span>
+                                </InputGroupAddon>
+                                <Input placeholder="Username" />
+                            </InputGroup>
+                            <InputGroup>
+                                <InputGroupAddon addonType="prepend">
+                                <span className="input-group-text" id="basic-addon">
+                                    <i className="fas fa-lock"></i>
+                                </span>
+                                </InputGroupAddon>
+                                <Input placeholder="******" />
+                            </InputGroup>
+                            <button className="login-form-btn">Login</button>
+                        </form>
+                    </div>
+                </div>
             </Fragment>
         )
     }
